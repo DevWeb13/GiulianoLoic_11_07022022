0426ab43a2e515536677c520fc7721cd920cf572
DevWeb13 <giuliano.loic@sfr.fr>
2022-02-14 10:23:52 +0100

    feat(HousingSheets + Gallery): Gallery terminé + titre, lieux, tags

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Gallery = ({ housingPictures }) => {
+  const [chooseImg, setChooseImg] = useState(0)
+  let currentPicture = housingPictures[chooseImg]
+
+  const previousImg = () => {
+    console.log(chooseImg)
+    if (chooseImg === 0) {
+      setChooseImg(housingPictures.length - 1)
+    } else {
+      setChooseImg(chooseImg - 1)
+    }
+    currentPicture = housingPictures[chooseImg]
+  }
+
+  const nextImg = () => {
+    if (chooseImg === housingPictures.length - 1) {
+      setChooseImg(0)
+    } else {
+      setChooseImg(chooseImg + 1)
+    }
+    currentPicture = housingPictures[chooseImg]
+  }
+
   return (
-    <div className="card">
-      <img src={housingPictures[0]} alt="" />
+    <div className="card gallery">
+      <button
+        className="arrow"
+        id="leftArrow"
+        type="button"
+        onClick={previousImg}
+      ></button>
+      <img src={currentPicture} alt="" />
+      <button
+        className="arrow"
+        id="rightArrow"
+        type="button"
+        onClick={nextImg}
+      ></button>
+      <p>
+        {chooseImg + 1}/{housingPictures.length}
+      </p>
     </div>
   )
 }
--- src/pages/HousingSheet/HousingSheet.jsx
+++ src/pages/HousingSheet/HousingSheet.jsx
@@ -1,22 +1,22 @@
 import { useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
 import { getHousing } from '../../services/dataManager'
 import Gallery from '../../components/Gallery/Gallery'
 
 function HousingSheet() {
   const { housingId } = useParams()
 
-  const [housingPictures, setHousingPictures] = useState({})
-
-  useEffect(() => {
-    setHousingPictures(getHousing(housingId).pictures)
-  }, [housingId])
+  const housingDatas = getHousing(housingId)
 
   return (
     <div className="housing-sheet">
-      <Gallery housingPictures={housingPictures} />
-      <h1>Fiche Logement 🧮</h1>
-      <h2>Logement numero : {housingId}</h2>
+      <Gallery housingPictures={housingDatas.pictures} />
+      <h1>{housingDatas.title}</h1>
+      <h2>{housingDatas.location}</h2>
+      <div className="tagsContainer">
+        {housingDatas.tags.map((tag) => (
+          <div className="tag">{tag}</div>
+        ))}
+      </div>
     </div>
   )
 }
